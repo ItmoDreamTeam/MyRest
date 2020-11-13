5260cecc078a03f37d2aa09a688b5dc872cf3691
Kirill Smirnov <smirnowku@gmail.com>
2020-11-13 14:43:18 +0300

    WIP: implement user sign-up
    On branch MR-1-user

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/service/user/UserServiceImpl.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/service/user/UserServiceImpl.kt
@@ -1,5 +1,9 @@
 package org.itmodreamteam.myrest.server.service.user
 
+import org.itmodreamteam.myrest.server.error.UserException
+import org.itmodreamteam.myrest.server.model.user.Identifier
+import org.itmodreamteam.myrest.server.model.user.User
+import org.itmodreamteam.myrest.server.repository.user.IdentifierRepository
 import org.itmodreamteam.myrest.server.repository.user.UserRepository
 import org.itmodreamteam.myrest.server.service.sms.SmsService
 import org.itmodreamteam.myrest.shared.user.SignUp
@@ -8,10 +12,34 @@ import org.springframework.stereotype.Service
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
+    private val identifierRepository: IdentifierRepository,
     private val smsService: SmsService,
 ) : UserService {
 
     override fun signUp(signUp: SignUp) {
-        TODO("Not yet implemented")
+        val identifier = getSignUpIdentifier(signUp)
+        sendVerificationMessage(identifier)
+    }
+
+    private fun getSignUpIdentifier(signUp: SignUp): Identifier {
+        val identifier = identifierRepository.findByValue(signUp.phone)
+        if (identifier == null) {
+            val user = userRepository.save(User(signUp.firstName, signUp.lastName))
+            return identifierRepository.save(Identifier(signUp.phone, Identifier.Type.PHONE, user))
+        }
+        val existingUser = identifier.user
+        if (!existingUser.enabled) {
+            existingUser.firstName = signUp.firstName
+            existingUser.lastName = signUp.lastName
+            userRepository.save(existingUser)
+            return identifier
+        }
+        throw UserException("Пользователь с данным номером телефона уже зарегистрирован")
+    }
+
+    private fun sendVerificationMessage(identifier: Identifier) {
+        val verificationCode = identifier.updateVerificationCode()
+        val text = "Для продолжения регистрации на сервисе MyRest введите код подтверждения: $verificationCode"
+        smsService.send(identifier.value, text)
     }
 }
