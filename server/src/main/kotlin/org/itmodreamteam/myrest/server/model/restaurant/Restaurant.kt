5345136d3d21295bf0f50797cb7d806834dc2c6c
Kirill Smirnov <smirnowku@gmail.com>
2020-11-16 21:35:30 +0300

    Add Restaurant Entity and Repository
    On branch MR-5-restaurant

---
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/model/restaurant/Restaurant.kt
@@ -0,0 +1,51 @@
+package org.itmodreamteam.myrest.server.model.restaurant
+
+import org.itmodreamteam.myrest.server.model.JpaEntity
+import org.itmodreamteam.myrest.shared.restaurant.RestaurantStatus
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "restaurants")
+class Restaurant() : JpaEntity() {
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    lateinit var status: RestaurantStatus
+
+    @NotNull
+    @Size(max = 50)
+    lateinit var name: String
+
+    @NotNull
+    @Size(max = 10000)
+    lateinit var description: String
+
+    @NotNull
+    @Size(max = 1000)
+    lateinit var legalInfo: String
+
+    @Size(max = 200)
+    var websiteUrl: String? = null
+
+    @Size(max = 50)
+    var phone: String? = null
+
+    @Size(max = 50)
+    var email: String? = null
+
+    var internalRating: Double = 0.0
+
+    var externalRating: Double = 0.0
+
+    constructor(name: String, description: String, legalInfo: String) : this() {
+        this.name = name
+        this.description = description
+        this.legalInfo = legalInfo
+        this.status = RestaurantStatus.PENDING
+    }
+}
---
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/repository/restaurant/RestaurantRepository.kt
@@ -0,0 +1,11 @@
+package org.itmodreamteam.myrest.server.repository.restaurant
+
+import org.itmodreamteam.myrest.server.model.restaurant.Restaurant
+import org.itmodreamteam.myrest.server.repository.JpaEntityRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface RestaurantRepository : JpaEntityRepository<Restaurant> {
+
+    fun findByNameLike(nameLike: String, pageable: Pageable): Page<Restaurant>
+}
