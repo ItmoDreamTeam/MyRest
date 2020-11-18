bf089aa7d68d1a7b16770ed3b6eed7a4592e66ba
Kirill Smirnov <smirnowku@gmail.com>
2020-11-18 21:18:07 +0300

    Declare NotificationService interface
    and add stub implementation

---
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/service/notification/StubNotificationService.kt
@@ -0,0 +1,15 @@
+package org.itmodreamteam.myrest.server.service.notification
+
+import org.itmodreamteam.myrest.server.model.user.User
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class StubNotificationService : NotificationService {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun notify(user: User, text: String) {
+        log.info("Notifying user ID=${user.id}. Message: $text")
+    }
+}
