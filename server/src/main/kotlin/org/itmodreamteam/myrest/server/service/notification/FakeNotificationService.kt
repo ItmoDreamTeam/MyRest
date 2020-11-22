a61663cd22c00e27a09d7528f49c0fda27b6ddf4
Kirill Smirnov <smirnowku@gmail.com>
2020-11-22 12:37:05 +0300

    Rename StubNotificationService using the term Fake
    On branch MR-11-employees

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/service/notification/FakeNotificationService.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/service/notification/FakeNotificationService.kt
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class StubNotificationService : NotificationService {
+class FakeNotificationService : NotificationService {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
