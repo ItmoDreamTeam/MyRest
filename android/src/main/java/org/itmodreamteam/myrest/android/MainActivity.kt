c6507efdde623fa7f8b2575cc96d418d2dc83f5c
Kirill Smirnov <smirnowku@gmail.com>
2020-11-27 01:08:44 +0300

    Add RestaurantSearchActivity

--- android/src/main/java/org/itmodreamteam/myrest/android/MainActivity.kt
+++ android/src/main/java/org/itmodreamteam/myrest/android/MainActivity.kt
@@ -1,11 +1,16 @@
 package org.itmodreamteam.myrest.android
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.itmodreamteam.myrest.android.restaurant.RestaurantSearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, RestaurantSearchActivity::class.java)
+        startActivity(intent)
     }
 }
