a808e16a3b2e560a995606e31dd2f3d733f169c5
Kirill Smirnov <smirnowku@gmail.com>
2020-12-02 19:14:41 +0300

    Move HttpClient init into shared module
    On branch MR-22-api-consumption

--- shared/src/commonMain/kotlin/org/itmodreamteam/myrest/shared/restaurant/RestaurantControllerImpl.kt
+++ shared/src/commonMain/kotlin/org/itmodreamteam/myrest/shared/restaurant/RestaurantControllerImpl.kt
@@ -1,13 +1,13 @@
 package org.itmodreamteam.myrest.shared.restaurant
 
-import io.ktor.client.*
 import io.ktor.client.request.*
 import org.itmodreamteam.myrest.shared.ContentPage
+import org.itmodreamteam.myrest.shared.HttpClientProvider
 import org.itmodreamteam.myrest.shared.Pageable
 
-class RestaurantControllerImpl(
-    private val client: HttpClient,
-) : RestaurantController {
+class RestaurantControllerImpl() : RestaurantController {
+
+    private val client = HttpClientProvider.provide()
 
     override suspend fun search(keyword: String, pageable: Pageable): ContentPage<RestaurantInfo> {
         return try {
