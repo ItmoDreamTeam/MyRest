b6f427eeea33575c310a348046bbf078bea2170e
Kirill Smirnov <smirnowku@gmail.com>
2020-12-02 19:14:41 +0300

    Move HttpClient init into shared module
    On branch MR-22-api-consumption

--- android/src/main/java/org/itmodreamteam/myrest/android/restaurant/search/RestaurantSearchViewModel.kt
+++ android/src/main/java/org/itmodreamteam/myrest/android/restaurant/search/RestaurantSearchViewModel.kt
@@ -3,26 +3,13 @@ package org.itmodreamteam.myrest.android.restaurant.search
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import io.ktor.client.*
-import io.ktor.client.features.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
 import kotlinx.coroutines.launch
-import org.itmodreamteam.myrest.shared.ClientProperties
 import org.itmodreamteam.myrest.shared.Pageable
 import org.itmodreamteam.myrest.shared.restaurant.RestaurantController
 import org.itmodreamteam.myrest.shared.restaurant.RestaurantControllerImpl
 
 class RestaurantSearchViewModel(
-    private val restaurantController: RestaurantController = RestaurantControllerImpl(
-        HttpClient {
-            defaultRequest {
-                host = ClientProperties.Server.host
-                port = ClientProperties.Server.port
-            }
-            install(JsonFeature)
-        }
-    ),
+    private val restaurantController: RestaurantController = RestaurantControllerImpl(),
 ) : ViewModel() {
 
     fun search(keyword: String, pageable: Pageable) = viewModelScope.launch {
