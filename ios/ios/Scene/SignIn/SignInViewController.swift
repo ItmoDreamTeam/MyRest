2ec22026b003467b10751d1d32cebba7f999d80b
rznov <rznnova@gmail.com>
2020-12-03 02:46:55 +0300

    Declare SignIn scene, implement PhoneTextField

---
+++ ios/ios/Scene/SignIn/SignInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignInViewController.swift
+//  ios
+//
+//  Created by Артем Розанов on 03.12.2020.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import UIKit
+
+protocol SignInView: UIViewController {}
+
+final class SignInViewController: UIViewController, SignInView {
+
+  init(router: RestaurantListRouter) {
+    
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
