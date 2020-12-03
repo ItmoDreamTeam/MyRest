206ee05c1e38bcd9f94784c0486f15dad26ca32d
Kirill Smirnov <smirnowku@gmail.com>
2020-12-04 01:34:42 +0300

    Wrap session verification into InvalidBearerTokenException
    On branch MR-31-user-controller

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/security/UserAuthenticationManager.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/security/UserAuthenticationManager.kt
@@ -4,6 +4,7 @@ import org.itmodreamteam.myrest.server.service.user.UserService
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.server.resource.BearerTokenAuthenticationToken
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,7 +14,11 @@ class UserAuthenticationManager(private val userService: UserService) : Authenti
         if (authentication !is BearerTokenAuthenticationToken) {
             throw RuntimeException("Unexpected Authentication: $authentication")
         }
-        val profile = userService.verifySession(authentication.token)
-        return UserAuthentication(profile)
+        try {
+            val profile = userService.verifySession(authentication.token)
+            return UserAuthentication(profile)
+        } catch (e: Exception) {
+            throw InvalidBearerTokenException(e.message, e)
+        }
     }
 }
