2390da36d80c94feced01078760a485973d51dfb
Kirill Smirnov <smirnowku@gmail.com>
2020-12-04 01:52:52 +0300

    Add UserClient
    On branch MR-31-user-controller

---
+++ shared/src/commonMain/kotlin/org/itmodreamteam/myrest/shared/user/UserClientImpl.kt
@@ -0,0 +1,36 @@
+package org.itmodreamteam.myrest.shared.user
+
+import io.ktor.client.request.*
+import org.itmodreamteam.myrest.shared.HttpClientProvider
+
+class UserClientImpl : UserClient {
+
+    private val client = HttpClientProvider.provide()
+
+    override suspend fun getMe(): Profile {
+        return client.get {
+            url("/users/me")
+        }
+    }
+
+    override suspend fun signUp(signUp: SignUp) {
+        return client.post {
+            url("/users/sign-up")
+            body = signUp
+        }
+    }
+
+    override suspend fun signIn(signIn: SignIn) {
+        return client.post {
+            url("/users/sign-in")
+            body = signIn
+        }
+    }
+
+    override suspend fun startSession(signInVerification: SignInVerification): ActiveSession {
+        return client.put {
+            url("/users/sessions/verification")
+            body = signInVerification
+        }
+    }
+}
