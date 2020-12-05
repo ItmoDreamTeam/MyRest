dac5dfceb4420c2112e89f87a62626dc8363efa3
Kirill Smirnov <smirnowku@gmail.com>
2020-12-05 19:25:51 +0300

    Create registration method of RestaurantController
    On branch MR-33-restaurant-controller

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/security/CurrentUserService.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/security/CurrentUserService.kt
@@ -1,7 +1,9 @@
 package org.itmodreamteam.myrest.server.security
 
+import org.itmodreamteam.myrest.server.model.user.User
 import org.itmodreamteam.myrest.shared.user.Profile
 
 interface CurrentUserService {
     val currentUser: Profile
+    val currentUserEntity: User
 }
