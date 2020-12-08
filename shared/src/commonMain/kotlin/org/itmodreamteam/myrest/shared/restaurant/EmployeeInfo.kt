6e16fcd5acceab46a6719c03a25243c4e1015c40
Kirill Smirnov <smirnowku@gmail.com>
2020-12-09 02:30:43 +0300

    Mark all DTOs with @Serializable

--- shared/src/commonMain/kotlin/org/itmodreamteam/myrest/shared/restaurant/EmployeeInfo.kt
+++ shared/src/commonMain/kotlin/org/itmodreamteam/myrest/shared/restaurant/EmployeeInfo.kt
@@ -1,7 +1,9 @@
 package org.itmodreamteam.myrest.shared.restaurant
 
+import kotlinx.serialization.Serializable
 import org.itmodreamteam.myrest.shared.user.Profile
 
+@Serializable
 data class EmployeeInfo(
     val id: Long,
     val restaurant: RestaurantInfo,
