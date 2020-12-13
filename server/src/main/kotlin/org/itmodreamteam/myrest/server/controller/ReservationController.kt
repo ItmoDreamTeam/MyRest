1cc1566679808836c85509a945ff0eb93e8cc0b8
Kirill Smirnov <smirnowku@gmail.com>
2020-12-13 22:39:11 +0300

    Add API methods to ReservationController
    On branch MR-47

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/controller/ReservationController.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/controller/ReservationController.kt
@@ -1,6 +1,8 @@
 package org.itmodreamteam.myrest.server.controller
 
 import org.itmodreamteam.myrest.server.service.reservation.ReservationReportService
+import org.itmodreamteam.myrest.server.service.restaurant.ReservationViewService
+import org.itmodreamteam.myrest.shared.restaurant.ReservationInfo
 import org.springframework.core.io.Resource
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.format.annotation.DateTimeFormat.ISO
@@ -9,11 +11,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/restaurants/{restaurantId}/reservations")
 class ReservationController(
     private val reservationReportService: ReservationReportService,
+    private val reservationViewService: ReservationViewService,
 ) {
 
     @GetMapping("/reports")
@@ -27,5 +31,34 @@ class ReservationController(
             .header("Content-Disposition", "inline")
             .contentType(MediaType.APPLICATION_PDF)
             .body(content)
+    }
+
+    @PutMapping
+    fun submitReservationForApproval(
+        @RequestParam tableId: Long,
+        @RequestParam activeFrom: LocalDateTime,
+        @RequestParam activeUntil: LocalDateTime,
+    ): ReservationInfo {
+        return reservationViewService.submitReservationForApproval(tableId, activeFrom, activeUntil)
+    }
+
+    @PutMapping("/{reservationId}/reject")
+    fun reject(@PathVariable reservationId: Long): ReservationInfo {
+        return reservationViewService.reject(reservationId)
+    }
+
+    @PutMapping("/{reservationId}/approve")
+    fun approve(@PathVariable reservationId: Long): ReservationInfo {
+        return reservationViewService.approve(reservationId)
+    }
+
+    @PutMapping("/{reservationId}/start")
+    fun start(@PathVariable reservationId: Long): ReservationInfo {
+        return reservationViewService.start(reservationId)
+    }
+
+    @PutMapping("/{reservationId}/complete")
+    fun complete(@PathVariable reservationId: Long): ReservationInfo {
+        return reservationViewService.complete(reservationId)
     }
 }
