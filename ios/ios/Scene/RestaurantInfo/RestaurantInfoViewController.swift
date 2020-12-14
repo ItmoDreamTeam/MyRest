6e07b28f3fd7b879e7bb7d628cdc78b1fc3c4195
rznov <rznnova@gmail.com>
2020-12-15 01:06:29 +1100

    Implement RestaurantViewController

---
+++ ios/ios/Scene/RestaurantInfo/RestaurantInfoViewController.swift
@@ -0,0 +1,58 @@
+//
+//  RestaurantInfoViewController.swift
+//  ios
+//
+//  Created by Артем Розанов on 14.12.2020.
+//  Copyright © 2020 orgName. All rights reserved.
+//
+
+import UIKit
+
+protocol RestaurantInfoView: UIViewController {}
+
+final class RestaurantInfoViewController: UIViewController, RestaurantInfoView {
+
+  private let photosCollectionViewHeight: CGFloat = 70
+
+  private var photosCollectionView: PhotosCollectionView
+  private var tableView: UITableView
+
+  init() {
+    photosCollectionView = PhotosCollectionView()
+    tableView = UITableView()
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+
+  private func configureNavBar() {
+    navigationItem.title = "MyRest"
+    navigationItem.largeTitleDisplayMode = .always
+    navigationController?.navigationBar.prefersLargeTitles = true
+  }
+
+  private func configureCollectionView() {
+    view.addSubview(photosCollectionView)
+    photosCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    photosCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    photosCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    photosCollectionView.heightAnchor.constraint(equalToConstant: photosCollectionViewHeight).isActive = true
+  }
+
+  private func configureTableView() {
+    view.addSubview(tableView)
+    tableView.topAnchor.constraint(equalTo: photosCollectionView.bottomAnchor).isActive = true
+    tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    tableView.separatorStyle = .none
+    tableView.showsVerticalScrollIndicator = false
+  }
+}
