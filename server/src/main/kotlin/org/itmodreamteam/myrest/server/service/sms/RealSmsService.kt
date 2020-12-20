d9799a2a4d993d716864323a0655067b32088dee
Kirill Smirnov <smirnowku@gmail.com>
2020-12-20 22:51:06 +0300

    Implement RealSmsService

--- server/src/main/kotlin/org/itmodreamteam/myrest/server/service/sms/RealSmsService.kt
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/service/sms/RealSmsService.kt
@@ -1,18 +1,66 @@
 package org.itmodreamteam.myrest.server.service.sms
 
+import org.itmodreamteam.myrest.server.error.UserException
 import org.slf4j.LoggerFactory
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
+import org.springframework.web.client.postForEntity
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Service
 @Primary
 @Profile("prod")
-class RealSmsService : SmsService {
+class RealSmsService(private var smsMessageRepository: SmsMessageRepository) : SmsService {
+
+    companion object {
+        private val MIN_INTERVAL_BETWEEN_MESSAGES = Duration.ofMinutes(5)
+    }
 
     private val log = LoggerFactory.getLogger(javaClass)
 
+    private val restTemplate = RestTemplateBuilder()
+        .rootUri("https://rest-api.d7networks.com/secure")
+        .build()
+
     override fun send(phone: String, text: String) {
         log.info("Sending SMS to $phone. Message: $text")
+        checkFrequencyLimit()
+        val message = registerMessage(phone, text)
+        executeSending(message)
     }
+
+    private fun checkFrequencyLimit() {
+        val pageable = PageRequest.of(0, 1, Sort.by("created").descending())
+        val history = smsMessageRepository.findAll(pageable).content
+        if (history.isNotEmpty()) {
+            val timePassed = Duration.between(history.first().created, LocalDateTime.now())
+            if (timePassed < MIN_INTERVAL_BETWEEN_MESSAGES) {
+                throw UserException("sms.limit.frequency")
+            }
+        }
+    }
+
+    private fun registerMessage(phone: String, text: String): SmsMessage {
+        return smsMessageRepository.save(SmsMessage(phone, text))
+    }
+
+    private fun executeSending(message: SmsMessage) {
+        try {
+            restTemplate.postForEntity<Any>("/send", SmsRequest(to = message.phone, content = message.text))
+        } catch (e: Exception) {
+            log.error("Unable to send SMS: ${e.message}", e)
+            throw UserException("sms.failure")
+        }
+    }
+
+    private data class SmsRequest(
+        val to: String,
+        val content: String,
+        val from: String = "MyRest",
+    )
 }
