fe1f5e7c698e2cfd854cea3633702fa800e22821
Kirill Smirnov <smirnowku@gmail.com>
2021-01-08 11:41:22 +0300

    MR-58: SMS (#62)
    * Create RealSmsService
    
    * Add SmsMessage entity
    
    * Implement RealSmsService
    
    * Shorten sign-up verification message to save money
    
    * Implement SMS daily limit check
    
    * Fix sign-up verification SMS test
    
    * Configure SMS service credentials
    
    * Implement Russian text messages sending (though, transliterated)
    
    * Add errors messages
    
    * Add incorrect phone error message

---
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/service/sms/SmsMessageRepository.kt
@@ -0,0 +1,5 @@
+package org.itmodreamteam.myrest.server.service.sms
+
+import org.itmodreamteam.myrest.server.repository.JpaEntityRepository
+
+interface SmsMessageRepository : JpaEntityRepository<SmsMessage>
