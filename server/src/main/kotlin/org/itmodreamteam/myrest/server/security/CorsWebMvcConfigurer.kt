bc2cda996d179db2cb3d33ba68990437c9be16a7
Kirill Smirnov <smirnowku@gmail.com>
2021-01-16 23:37:05 +0300

    Configure CORS

---
+++ server/src/main/kotlin/org/itmodreamteam/myrest/server/security/CorsWebMvcConfigurer.kt
@@ -0,0 +1,15 @@
+package org.itmodreamteam.myrest.server.security
+
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Component
+class CorsWebMvcConfigurer : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
