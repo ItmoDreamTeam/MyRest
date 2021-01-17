6f75eaa7a7f0e62f299ec572109d994138e41441
rznov <rznnova@gmail.com>
2021-01-18 00:11:07 +1100

    Add more util for Date

--- ios/ios/Utils/Date+Utils.swift
+++ ios/ios/Utils/Date+Utils.swift
@@ -7,13 +7,26 @@
 //
 
 import Foundation
+import shared
 
 extension Date {
-    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
-        return calendar.dateComponents(Set(components), from: self)
-    }
+  func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+    return calendar.dateComponents(Set(components), from: self)
+  }
 
-    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
-        return calendar.component(component, from: self)
-    }
+  func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+    return calendar.component(component, from: self)
+  }
+
+  func toKotlindatetimeLocalDateTime() -> Kotlinx_datetimeLocalDateTime {
+    Kotlinx_datetimeLocalDateTime(
+      year: Int32(self.get(.year)),
+      monthNumber: Int32(self.get(.month)),
+      dayOfMonth: Int32(self.get(.day)),
+      hour: Int32(self.get(.hour)),
+      minute: Int32(self.get(.minute)),
+      second: Int32(self.get(.second)),
+      nanosecond: Int32(self.get(.nanosecond))
+    )
+  }
 }
